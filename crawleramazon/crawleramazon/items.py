964064cf78d2fd44e261b43f1cde3a2f5ce0825e
Nabila Sarvari <nabilasarwary@abv.bg>
2022-12-28 23:13:58 +0200

    items.py_updated

--- crawleramazon/crawleramazon/items.py
+++ crawleramazon/crawleramazon/items.py
@@ -9,4 +9,8 @@ import scrapy
 class CrawleramazonItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
+    product_name = scrapy.Field()
+    product_author = scrapy.Field()
+    product_price = scrapy.Field()
+    product_imageId = scrapy.Field()
     pass
--- crawleramazon/crawleramazon/spiders/amazon_spider.py
+++ crawleramazon/crawleramazon/spiders/amazon_spider.py
@@ -3,7 +3,7 @@ import scrapy
 
 class AmazonSpiderSpider(scrapy.Spider):
     name = 'amazon_spider'
-    start_urls = ['http://amazon.com/']
+    start_urls = ['https://www.amazon.de/s?k=Books&crid=2ZC63KF6C6HCR&sprefix=books%2Caps%2C160&ref=nb_sb_noss_1']
 
     def parse(self, response):
         pass
